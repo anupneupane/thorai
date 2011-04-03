f4d13a643f4632ae01a497407d89891bcf4c7d1d
Vinaya Shrestha <vinayashrestha@gmail.com>
2011-04-03 17:15:27 -0500

    Referral removed from db:migrate

--- db/migrate/20110204062229_add_used_up_to_referral.rb
+++
@@ -1,9 +0,0 @@
-class AddUsedUpToReferral < ActiveRecord::Migration
-  def self.up
-    add_column :referrals, :used_up, :boolean
-  end
-
-  def self.down
-    remove_column :referrals, :used_up
-  end
-end
