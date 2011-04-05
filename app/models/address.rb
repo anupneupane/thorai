a273eec70dd5358f5fb505a6591c616f48525682
Vinaya Shrestha <vinayashrestha@gmail.com>
2011-04-04 20:55:43 -0500

    Address model changed

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ActiveRecord::Base
   attr_accessible :contact_information_id, :street_address, :city, :state, :zip_code
   belongs_to :contact_information
   
-  validates_presence_of :street_address, :city, :state, :zip_code
+  #validates_presence_of :street_address, :city, :state, :zip_code
   
 end
