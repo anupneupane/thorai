6512b74bc59d3862130528cbc2d4852a53ed87d0
Vinaya Shrestha <vinayashrestha@gmail.com>
2011-07-21 16:50:51 -0500

    All committed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
       @user = user
       mail( :to => user.email,
             :subject => "Registration Confirmation",
-            :from => "thorai@gmail.com"
+            :from => "thorai.team@gmail.com"
           )
     end
 
@@ -12,14 +12,14 @@ class UserMailer < ActionMailer::Base
       @user = user
       mail( :to => "#{user.profile.first_name} #{user.profile.last_name} <#{user.profile.contact_information.email}>",
             :subject => "Purchase Confirmation",
-            :from => "thorai@gmail.com"
+            :from => "thorai.team@gmail.com"
           )
     end
 
     def daily_deal_mail(daily_deal_mailing_list, deal)
       mail( :to => daily_deal_mailing_list.user_email,
             :subject => deal.title,
-            :from => "thorai@gmail.com"
+            :from => "thorai.team@gmail.com"
           )
     end
     
@@ -29,7 +29,7 @@ class UserMailer < ActionMailer::Base
       @signup_url = signup_url
       mail( :to => invitation.recipient_email,
             :subject => "#{@sender.profile.first_name} #{@sender.profile.last_name} invites you to thorai",
-            :from => "thorai@gmail.com"
+            :from => "thorai.team@gmail.com"
           )
       invitation.update_attribute(:sent_at, Time.now)
     end
